8c8065a34a275f7759ebbd3c1a3988a1c8eb1d83
ara1511 <ararejala15@gmail.com>
2024-09-19 19:59:25 -0300

    Creacion de la entidad libro

---
+++ models/libro.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import UUID, func
+
+Base = declarative_base()
+
+class Libro(Base):
+    __tablename__ = 'libros'
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String)
+    autor = Column(String)
+    publicado_en = Column(Date)
+    isbn = Column(String)
