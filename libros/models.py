c873d36b2bf5c95ea98e932c8e18eed46f50fb6d
dario-coronel <dario.coronel@gmail.com>
2024-09-22 16:46:22 -0300

    Creacion modelo de la clase Libro

---
+++ libros/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Date, UUID
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+Base = declarative_base()
+
+class Libro(Base):
+    __tablename__ = "libros"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    titulo = Column(String, nullable=False)
+    autor = Column(String, nullable=False)
+    publicado_en = Column(Date, nullable=False)
+    isbn = Column(String, nullable=False, unique=True)
