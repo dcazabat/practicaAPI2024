135f1bf96121a2e9692008bb3312600cdcfc2eae
G6aspar <cgaspar025@gmail.com>
2024-09-23 19:41:05 -0300

    creacion de main y env

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from default.defaultmodel import Base
+from config.cnx import engine
+
+from default.routes import default
+from libro.routes import libro
+
+app = FastAPI(
+    title="API Rest FastAPI",
+    description="Ejemplo de API Rest con FASTAPI",
+    version="0.0.1",
+        contact={
+        "name": "Soporte Técnico",
+        "email": "soporte@miempresa.com",
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    }
+)
+
+app.include_router(default, prefix='', tags=['App Routes Default'])
+app.include_router(libro, prefix='/libros', tags=['Libros Endpoints'])
+
+Base.metadata.create_all(bind=engine)
