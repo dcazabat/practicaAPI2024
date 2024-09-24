40be061451e4bf9289491ead65b248c071dee585
G6aspar <cgaspar025@gmail.com>
2024-09-23 21:55:21 -0300

    post

--- libro/routes.py
+++ libro/routes.py
@@ -25,3 +25,13 @@ def get_libro(id: UUID):
         return JSONResponse(content='Libro no encontrado', status_code=404)
     except Exception as e:
         raise HTTPException(detail=f'Error al recuperar libro: {e}', status_code=500)
+    
+@libro.post('/', response_model=LibroDTO, status_code=201, summary="Crear un nuevo libro", description="Crea un nuevo libro en el sistema con los datos proporcionados.", tags=["Libros"], responses={201: {"description": "Libro creado con éxito"}, 404: {"description": "Libro no creado"}, 500: {"description": "Error interno del servidor"}})
+def create_libro(libropost: CreateLibro):
+    try:
+        libro_new = CreateLibro(libro=libropost)
+        if libro_new:
+            return libro_new
+        return JSONResponse(content='Libro no creado', status_code=404)
+    except Exception as e:
+        raise HTTPException(detail=f'Error al crear libro: {e}', status_code=500)
