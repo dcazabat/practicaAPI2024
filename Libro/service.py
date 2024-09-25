0f82660303ad3064379922a360a0ebe2be05190e
franciscisco perez <franciscoperez615570.gmail.com>
2024-09-25 17:09:02 -0300

    Creamos la funcion crear libro.

--- Libro/service.py
+++ Libro/service.py
@@ -6,3 +6,11 @@ class LibroService:
     def __init__(self, session: Session):
         self.session = session
 
+    def crear_libro(self, libro_create: LibroCreate):
+        libro = Libro(**libro_create.dict())
+        self.session.add(libro)
+        self.session.commit()
+        self.session.refresh(libro)
+        return libro
+
+
